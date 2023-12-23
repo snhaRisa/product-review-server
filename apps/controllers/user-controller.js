42a5f0df985ac87c4b6145747866f04cead3873a
Rishav <risasnha1999@gmail.com>
2023-12-24 00:32:20 +0530

    userRoute & Image-upload functionality

--- apps/controllers/user-controller.js
+++ apps/controllers/user-controller.js
@@ -1,25 +1,34 @@
 
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cloudinary = require('cloudinary').v2;
 const { validationResult } = require('express-validator');
 
 const userModel = require('../models/user-model');
+// const configureCloudinary = require('../../configuration/configureCloudinary');
 
 const userController = {}; 
 
-userController.register = async (request, response)=>
+//configuring cloudinary.
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY_CLOUD, 
+    api_secret: process.env.API_SECRET_CLOUD 
+});
+
+userController.register = async (req, res)=>
 {
-    const body = request.body; 
-    const errors = validationResult(request); 
+    const body = req.body; 
+    const errors = validationResult(req); 
 
     if(!errors.isEmpty())
     {
-        return response.status(400).json({errors: errors.array()});
+        return res.status(400).json({errors: errors.array()});
     }
 
     try
     {
-        const {username, email, password} = request.body; 
+        const {username, email, password} = req.body; 
         
         const salt = await bcryptjs.genSalt();
         const hashPassword = await bcryptjs.hash(password, salt);
@@ -27,23 +36,23 @@ userController.register = async (request, response)=>
         if(email === 'rishav99@gmail.com')
         {
             const newUserDoc = await userModel.create({username, email, password: hashPassword, role: 'admin'}); 
-            response.json('Successfully made you the Admin !');
+            res.json('Successfully made you the Admin !');
         }
         else
         {
             const newUserDoc = await userModel.create({username, email, password: hashPassword});
-            response.json('Successfully created the user !');    
+            res.json('Successfully created the user !');    
         }        
     }
     catch(err)
     {
-        response.status(400).json(err);
+        res.status(400).json(err);
     }
 };
 
-userController.login = async (request, response)=>
+userController.login = async (req, res)=>
 {
-    const body = request.body; 
+    const body = req.body; 
     try
     {
         const {email, password} = body; 
@@ -58,23 +67,99 @@ userController.login = async (request, response)=>
                 const tokenData = {
                     _id : checkUser._id
                 }; 
-                const token = jwt.sign(tokenData, process.env.SECRET_KEY); 
-                response.json(`Bearer ${token}`); 
+                const expiresIn = '1h';
+                const token = jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn}); 
+                res.json(`Bearer ${token}`); 
             }
             else
             {
-                response.status(400).json('Incorrect E-Mail or Password !');
+                res.status(400).json('Incorrect E-Mail or Password !');
             }
         }
         else
         {
-            response.status(400).json('Incorrect E-Mail or Password !');
+            res.status(400).json('Incorrect E-Mail or Password !');
         }
     }
     catch(err)
     {
-        response.status(400).json(err);
+        res.status(400).json(err);
     };
 };
+
+userController.getAccount = async (req, res)=>
+{
+    try
+    {
+        const {user} = req; 
+        const userId = user._id; 
+
+        const userDoc = await userModel.findOne({_id: userId});
+        if(userDoc)
+        {
+            const tempObj = {
+                _id: userDoc._id,
+                username: userDoc.username,
+                email: userDoc.email,
+                image: userDoc.image,
+                createdAt: userDoc.createdAt,
+                updatedAt: userDoc.updatedAt 
+            }
+            res.json(tempObj);
+        }
+        else
+        {
+            res.status(400).json({});
+        };
+    }
+    catch(err)
+    {
+        res.status(400).json('Access Denied !');
+    }
+};
+
+userController.uploadUserImage = async (req, res)=>
+{
+    //Was not able to use cors. Need to implement & learn correct architecture.
+    res.header("Access-Control-Allow-Origin", "*");
+ 
+    const file = req.files.userImage;
+    const tokenData = req.user; 
+    const userId = tokenData._id; 
+
+    if(!file || Object.keys(file).length === 0)
+    {
+        return res.status(400).json('No file selected !')
+    };
+    if(!file.mimetype.startsWith('image/'))
+    {
+        return res.status(400).json('Received file is not an Image !');
+    };
+
+    try
+    {   
+        const uploading = await cloudinary.uploader.upload(file.tempFilePath);
+        if(uploading.url)
+        {
+            const userDoc = await userModel.findByIdAndUpdate(userId, {image: uploading.url}, {new:true, runValidators: true}); 
+            const tempObj = {
+                _id: userDoc._id, 
+                username: userDoc.username, 
+                email: userDoc.email, 
+                createdAt: userDoc.createdAt, 
+                image: userDoc.image
+            }
+            res.json(tempObj);
+        }
+        else
+        {
+            res.status(400).json('Error uploading image!');
+        }
+    }
+    catch(err)
+    {
+        res.status(400).json('Error uploading image!');
+    }
+}
 
 module.exports = userController; 
