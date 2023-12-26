761d74c11b668a9223a021af8e3aabe67cb0ed4e
Rishav <risasnha1999@gmail.com>
2023-12-27 02:00:39 +0530

    Homepage, SingleContentPage, Adding comments, liking & disliking products or reviews.

--- apps/controllers/product-controller.js
+++ apps/controllers/product-controller.js
@@ -58,39 +58,87 @@ productController.addProduct = async (req, res)=>
     }
 };
 
-productController.updateProduct = async (req, res)=>
+productController.getProduct = async (req, res)=>
 {
-    res.header("Access-Control-Allow-Origin", "*");
-    const errors = validationResult(req); 
-    const file = req.files.productImage; 
-
-    if(!errors.isEmpty())
-    {
-        return res.status(400).json({errors:errors.array()});
-    };
-    if(!file || Object.keys(file).length===0)
-    {
-        return res.status(400).json('No File Uploaded.')
-    };
-    if(!file.mimetype.startsWith('image/'))
-    {
-        return res.status(400).json('Uploaded file is not an Image.');
-    };
-
     try
     {
-        const {productId, title, description, category} = req.body;
-        const upload = await cloudinary.uploader.upload(file.tempFilePath);
-        if(upload.url)
+        const productDocs = await ProductModel.find();
+        if(productDocs.length>0)
         {
-            const updateDoc = await ProductModel.findByIdAndUpdate(productId, {title, description, category, image:upload.url}, {runValidators:true, new: true}); 
-            res.json(updateDoc); 
+            res.json(productDocs);
         }
         else
         {
-            console.log('wh')
-            res.status(400).json('Error updating the product details.');
-        };
+            res.json({});
+        }
+    }
+    catch(err)
+    {
+        res.status(400).json('Error fetching the Products.');
+    }
+};
+
+productController.getOneProduct = async (req, res)=>
+{
+    try
+    {   
+        const {productId} = req.params; 
+        const productDoc = await ProductModel.findById(productId).populate('reviews.userId'); 
+        if(productDoc)
+        {
+            res.json(productDoc)
+        }
+        else
+        {
+            res.status(400).json({});
+        }
+    }
+    catch(err)
+    {
+        res.status(400).json('Could not fetch the document.');
+    }
+}
+
+productController.updateProduct = async (req, res)=>
+{
+    res.header("Access-Control-Allow-Origin", "*");
+ 
+    const {productId, titleTemp, categoryTemp, descriptionTemp, fileImg} = req.body;  
+    let file; 
+    
+    try
+    {
+        //If new image uploaded. Uploading it and updating. 
+        if(req.files && req.files.fileImg)
+        {
+            file = req.files.fileImg;
+
+            if(file)
+            {
+                if(!file.mimetype.startsWith('image/'))
+                {
+                    return res.status(400).json('Uploaded file is not an image.');
+                }
+
+                const upload = await cloudinary.uploader.upload(file.tempFilePath);
+                if(upload.url)
+                {
+                    const updateDoc = await ProductModel.findByIdAndUpdate(productId, {title: titleTemp, description: descriptionTemp, category: categoryTemp, image:upload.url}, {runValidators:true, new: true}); 
+                    const remainingDocs = await ProductModel.find(); 
+                    res.json(remainingDocs); 
+                }
+                else
+                {
+                    res.status(400).json('Error updating the product details.');
+                };
+            }
+        }
+        else  //If no image, uploading the url itself. 
+        {
+            const updateDoc = await ProductModel.findByIdAndUpdate(productId, {title: titleTemp, description: descriptionTemp, category: categoryTemp, image: fileImg}, {runValidators: true, new: true}); 
+            const remainingDocs = await ProductModel.find(); 
+            res.json(remainingDocs); 
+        }
     }
     catch(err)
     {
@@ -111,7 +159,116 @@ productController.deleteProduct = async (req, res)=>
     }
     catch(err)
     {
+        console.error(err);
         res.status(400).json('Error deleting the product.');
+    }
+};
+
+productController.addReview = async (req, res)=>
+{
+    try
+    {
+        const {productId, userId, rating, text} = req.body.tempObj; 
+        
+        const newReview = {
+            productId: productId, 
+            userId: userId, 
+            rating: rating, 
+            text: text, 
+            timestamp: Date.now(), 
+        };
+
+        const updateDoc = await ProductModel.findByIdAndUpdate(productId, 
+            {$push:{reviews: newReview}}, 
+            {new: true}
+        ).populate('reviews.userId');
+
+        res.json(updateDoc);
+    }
+    catch(err)
+    {
+        res.status(400).json('Error Adding Review.');
+    }
+};
+
+productController.addProductLike = async (req, res)=>
+{
+    try
+    {
+        const {productId, userId} = req.body; 
+
+        const updateDoc = await ProductModel.findByIdAndUpdate(productId, 
+            {$push: {likes: {userId: userId}}}, 
+            {new: true}
+        ).populate('reviews.userId'); 
+
+        res.json(updateDoc);
+    }
+    catch(err)
+    {
+        res.status(400).json('Error while liking the product.');
+    };
+};
+
+productController.addProductDislike = async (req, res)=>
+{
+    try
+    {
+        const {productId, userId} = req.body; 
+
+        const updateDoc = await ProductModel.findByIdAndUpdate(productId, 
+            {$push: {dislikes: {userId: userId}}}, 
+            {new: true}
+        ).populate('reviews.userId'); 
+
+        res.json(updateDoc);
+    }
+    catch(err)
+    {
+        res.status(400).json('Error while liking the product.');
+    };
+};
+
+productController.addReviewLike = async (req, res)=>
+{
+    try
+    {
+        const {productId, reviewId, userId} = req.body; 
+        const updatedProduct = await ProductModel.findOneAndUpdate(
+            {'reviews._id': reviewId, _id: productId},
+            {
+              $push: 
+              {
+                'reviews.$.likes': { userId: userId },
+              },
+            },
+            { new: true }
+        ).populate('reviews.userId');
+
+        res.json(updatedProduct);
+    }
+    catch(err)
+    {
+        res.status(400).json('Error liking the review.'); 
+    }
+};
+
+productController.addReviewDislike = async (req, res)=>
+{
+    try
+    {
+        const {productId, reviewId, userId} = req.body; 
+        const updatedProduct = await ProductModel.findOneAndUpdate(
+            { _id: productId, 'reviews._id': reviewId }, 
+            { $push: { 'reviews.$.dislikes': { userId: userId } } }, 
+            { new: true }
+        ).populate('reviews.userId');
+        
+        res.json(updatedProduct);
+    }
+    catch(err)
+    {
+        res.status(400).json('Error disliking the review.'); 
     }
 };
 
--- apps/middlewares/authorization.js
+++ apps/middlewares/authorization.js
@@ -5,33 +5,36 @@ const userModel = require('../models/user-model');
 const authorization = async (req, res, next)=>
 {
     let token = req.headers['authorization']; 
-    token = token.split(' ')[1]; 
 
     if(token)
     {
+        token = token.split(' ')[1];
+
         const tokenData = jwt.verify(token, process.env.SECRET_KEY); 
         const id = tokenData._id; 
         try
         {
             const checkAdmin = await userModel.findById(id); 
+
             if(checkAdmin && checkAdmin.role === 'admin')
             {
                 req.user = checkAdmin; 
-                next();
+                return next(); 
             }
             else
             {
-                res.status(400).json('Access Denied !'); 
+                return res.status(400).json('Access Denied !'); 
             }
         }
         catch(err)
         {
-            res.status(400).status('Access Denied !'); 
+            console.log('a')
+            return res.status(400).json('Access Denied !'); 
         }
     }
     else
     {
-        res.status(403).json('Access Denied !'); 
+        return res.status(403).json('Access Denied !'); 
     };
 };
 
